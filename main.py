64bb3c4081f53ab206a65fe9923ac100f6b92d6e
SRIJITA-tech <111190487+SRIJITA-tech@users.noreply.github.com>
2022-09-13 19:59:39 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
-# minicalculator
-# 1.add
-# 2.subtraction
-# 3.multiplication
-# 4.division
+candidate1 = input("Enter first candidate name:")
+candidate2 = input("Enter second candidate name:")
+cand1_votes = 0
+cand2_votes = 0
+voters_id = [101,102,103,104,105,106]
+no_of_voters = len(voters_id)
+print("number of voters:",no_of_voters)
+voted = []
+while True:
+    if voters_id == []:
+        print("voting is over")
+        if cand1_votes > cand2_votes:
+            print(f"{candidate1}won the election with{cand1_votes}")
+        elif cand2_votes > cand1_votes:
+            print(f"{candidate2}won the election with{cand2_votes}")
+        elif cand1_votes == cand2_votes:
+            print("tied!!!!!")
+            break
+        else:
+            voter = int(input("Enter your id"))
+            if voter in voted:
+                print("you already voted")
+            else:
+                if voter in voters_id:
+                    print(f"1.{candidate1}\n 2.{candidate2}")
+                    choice = int(input("enter your choice"))
+                    if choice == 1:
+                        cand1_votes +=1
+                        print(f"you voted{candidate1}")
+                    elif choice == 2:
+                        cand2_votes +=1
+                        print(f"you voted{candidate2}")
+                        voters_id.remove(voter)
+                        voted.append(voter)
+                    else:
+                        print("you are not allowed to vote")
 
-print("Select an operation to perform:")
-print("1.add")
-print("2.subtraction")
-print("3.multiplication")
-print("4.division")
 
-operation = input()
-if operation == "1":
-    num1 = input("Enter first number:")
-    num2 = input("Enter second number:")
-    print("the sum is " + str(int(num1)+int(num2)))
-elif operation == "2":
-    num1 = input("Enter first number:")
-    num2 = input("Enter second number:")
-    print("the difference is"+ str(int(num1)-int(num2)))
-elif operation == "3":
-    num1 = input("Enter first number:")
-    num2 = input("Enter second number:")
-    print("the product is"+ str(int(num1)*int(num2)))
-elif operation == "4":
-    num1 = input("Enter first number:")
-    num2 = input("Enter second number:")
-    print("the result is" + str(int(num1)/int(num2)))
-else:
-    print("invalid entry")
 
 
 
